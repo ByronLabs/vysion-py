f9b62465995c5085f4a6d6d257c4e9fe1cc826fd
Javier Junquera <javier@junquera.io>
2022-06-16 08:45:25 +0200

    Fixing package

--- setup.py
+++ setup.py
@@ -5,25 +5,24 @@ with open('requirements.txt') as f:
 
 print(requirements)
 
-
 # https://docs.python.org/3/distutils/setupscript.html
 setuptools.setup(
     name='vysion',
     version='0.0.1', # TODO Read from __version__
     description='The official Python client library for Vysion',
-    # license='Apache 2',
+    license='Apache 2',
     license_files = ('LICENSE'),
     # long_description=long_description,
     # long_description_content_type="text/markdown",
-    # url='https://github.com/VirusTotal/vt-py',
-    packages=['vysion', 'vysion.client'],
-    python_requires='>=3.6.0',
-    install_requires=requirements
+    url='https://gitlab.com/byronlabs/vysion/vysion-py',
+    packages=['vysion', 'vysion.client', 'vysion.model', 'vysion.taxonomy'],
+    python_requires='>=3.7.0',
+    install_requires=requirements,
     # setup_requires=['pytest-runner'],
     # extras_require={'test': ['pytest', 'pytest_httpserver', 'pytest_asyncio']},
-    # classifiers=[
-    #    'Programming Language :: Python :: 3',
-    #    'License :: OSI Approved :: Apache Software License',
-    #    'Operating System :: OS Independent',
-    #]
+    classifiers=[
+       'Programming Language :: Python :: 3',
+       'License :: OSI Approved :: Apache Software License',
+       'Operating System :: OS Independent',
+    ]
 )
