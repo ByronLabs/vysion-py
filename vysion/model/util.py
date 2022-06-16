c4fbee183fb8beec43503db79d019470dd4aface
Javier Junquera <javier@junquera.io>
2022-06-16 08:47:27 +0200

    Fixing headings

--- vysion/model/util.py
+++ vysion/model/util.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.10
+#!/usr/bin/env python3
 """
    Copyright 2022 ByronLabs S.L.
 
@@ -13,6 +13,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+
 """
 
 from typing import List
