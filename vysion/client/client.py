770ecee83a7d86130ce246109ac2a72f67aa17d7
Javier Junquera <javier@junquera.io>
2022-06-16 09:22:10 +0200

    Minor refactor

--- vysion/client/client.py
+++ vysion/client/client.py
@@ -24,6 +24,7 @@ import json
 from urllib.parse import urljoin, urlencode
 from pydantic import validate_arguments
 import requests
+from vysion.client.error import APIError
 
 import vysion.model as model
 from vysion.model.util import process_response
@@ -58,11 +59,11 @@ class VysionErrors:
 
 class Client():
 
-    def __init__(self, apiKey:str, headers: dict = dict(), proxy: dict=None):
-        
-        assert isinstance(apiKey, str), "API key MUST be a string"
+    def __init__(self, api_key: str, headers: dict = dict(), proxy: dict = None):
 
-        self.apiKey = apiKey
+        assert isinstance(api_key, str), "API key MUST be a string"
+
+        self.api_key = api_key
         self.proxy = proxy
         self.headers=headers
 
@@ -74,7 +75,7 @@ class Client():
 
             headers = self.headers.copy()
             headers.update({
-                "X-API-KEY": self.apiKey,
+                "X-API-KEY": self.api_key,
             })
 
             self._session = requests.Session()
@@ -157,122 +158,14 @@ class Client():
       session = self.__get_session__()
       r = session.get(url)
 
+      # TODO Improve this
+      if r.status_code != 200:
+        raise APIError(r.status_code, r.text)
+
       response = r.json()
       raw_hits = response.get('hits', [])
 
       return process_response(raw_hits)
 
-
-# https://vysion-api-secured-afkbm06.nw.gateway.dev/api/v1/email/purplefdw@protonmail.ch' \
-
-'''
-SAMPLE API's RESPONSE (20220613)
-$ curl --location --request GET 'https://vysion-api-secured-afkbm06.nw.gateway.dev/api/v1/email/purplefdw@protonmail.ch' --header 'Accept: application/json' --header 'x-api-key: *********************' | jq 
-
-{
-  "total": {
-    "value": 3,
-    "relation": "eq"
-  },
-  "max_score": null,
-  "hits": [
-    {
-      "_index": "vysion-062022",
-      "_id": "614d869a896a534dd40ac36d",
-      "_score": null,
-      "_source": {
-        "protocol": "http",
-        "domain": "trollodrome2.torpress2sarn7xw.onion",
-        "port": 80,
-        "path": "/topic/service-%f0%9f%92%80-hacking-2/",
-        "signature": "27749d192f4a9cdc603b1fd94cf599ad",
-        "parent": "421b5f3b1a8c8cd2f51dba70b658d278",
-        "date": "2021-10-21T08:53:43.626235",
-        "network": "tor",
-        "sha1sum": null,
-        "ssdeep": null,
-        "title": "SERVICE 💀 HACKING – Le Trollodrôme 2.0",
-        "url": "trollodrome2.torpress2sarn7xw.onion/topic/service-%f0%9f%92%80-hacking-2/",
-        "language": "fr",
-        "html": null,
-        "email": [
-          "purplefdw@protonmail.ch"
-        ],
-        "pastebin-dumps": [],
-        "skype": [],
-        "telegram": [],
-        "whatsapp": []
-      },
-      "sort": [
-        1634806423626
-      ]
-    },
-    {
-      "_index": "vysion-062022",
-      "_id": "614d868d896a534dd40ac212",
-      "_score": null,
-      "_source": {
-        "protocol": "http",
-        "domain": "trollodrome2.torpress2sarn7xw.onion",
-        "port": 80,
-        "path": "/topic/service-%f0%9f%92%80-hack/",
-        "signature": "28d781c04f157e07224744a589b6de67",
-        "parent": "421b5f3b1a8c8cd2f51dba70b658d278",
-        "date": "2021-10-21T08:53:43.561108",
-        "network": "tor",
-        "sha1sum": null,
-        "ssdeep": null,
-        "title": "SERVICE 💀 HACK – Le Trollodrôme 2.0",
-        "url": "trollodrome2.torpress2sarn7xw.onion/topic/service-%f0%9f%92%80-hack/",
-        "language": "fr",
-        "html": null,
-        "email": [
-          "purplefdw@protonmail.ch"
-        ],
-        "pastebin-dumps": [],
-        "skype": [],
-        "telegram": [],
-        "whatsapp": []
-      },
-      "sort": [
-        1634806423561
-      ]
-    },
-    {
-      "_index": "vysion-062022",
-      "_id": "614d8535896a534dd40aa47b",
-      "_score": null,
-      "_source": {
-        # URL
-        "protocol": "http",
-        "domain": "tor66sewebgixwhcqfnp5inzp5x5uohhdy3kvtnyfxc2e5mxiuh34iid.onion",
-        "port": 80,
-        "path": "/about",
-        "signature": "62ab8067d6d4fe91074283cd70767414",
-        "parent": "7c9de3f17e87b5d42d920ccddeac5a48",
-        "network": "tor",
-
-        "date": "2021-10-21T08:53:42.560068",
-        
-        "title": "Tor66 - About",
-        "language": "en",
-        "html": null,
-        "sha1sum": null,
-        "ssdeep": null,
-        "email": [
-          "torservice77@protonmail.ch"
-        ],
-        "pastebin-dumps": [],
-        "skype": [],
-        "telegram": [],
-        "whatsapp": []
-      },
-      "sort": [
-        1634806422560
-      ]
-    }
-  ]
-}
-'''
 
 # TODO /api/v1/feeds
