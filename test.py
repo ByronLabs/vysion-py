a4af19fb3f5248898ed7f09488f3ed2ada0694ce
Germán  Esteban López <alumnogerman@gmail.com>
2023-12-01 13:38:15 +0100

    Modified client.py to add others api_host

---
+++ test.py
@@ -0,0 +1,6 @@
+from vysion import client
+# 8O0mzMVw1w2MHr7bhnBAF5kazDKlXdTQ1yUO87j2
+# 4T1PpMsbmtaCpbIvg03pA7lBSrnq3Fqn58awVzQX
+c = client.Client(api_key="4T1PpMsbmtaCpbIvg03pA7lBSrnq3Fqn58awVzQX")
+
+print(c.search("contileaks"))
