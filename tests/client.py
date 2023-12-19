aadbb3a495e3e822817fec7c189686fc94d81327
Germán Esteban <38075180+German7463@users.noreply.github.com>
2023-12-19 14:07:13 +0100

    Update client.py

--- tests/client.py
+++ tests/client.py
@@ -1,11 +1,4 @@
-#from config import API_KEY
 from vysion import client
 import os
 
-API_KEY = "DcFnTtBuQC4OIdtXy3VAY4LOlKcw162K1tfDA9wd"
-
 c = client.Client(api_key=API_KEY)
-
-result = c.search("madrid.org")
-for hit in result.hits:
-    print(hit.page.title)
