a883123cf87c9ef32fe237bc8921f4ddb8893ff0
jgarl <juan.garcia@byronlabs.io>
2025-01-31 11:44:55 +0100

    Added Sector and Industry Group fields in dto

--- vysion/dto/dto.py
+++ vysion/dto/dto.py
@@ -225,6 +225,8 @@ class RansomwareHit(BaseModel):
     companyAddress: Optional[str] = None
     companyLink: Optional[str] = None
     country: Optional[str] = None
+    sector: Optional[str] = None
+    industryGroup: Optional[str] = None
 
 class DocumentHit(BaseModel):
     page: Page
@@ -401,6 +403,8 @@ class RansomFeedHit(BaseModel):
     detectionDate: datetime
     text: Optional[str]
     country: Optional[str]
+    sector: Optional[str]
+    industryGroup: Optional[str]
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
